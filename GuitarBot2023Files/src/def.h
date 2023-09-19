a5221287feb24cc5c0312ccef47d91b0395cc481
Shayahn Mirfendereski <shayahnmir@gmail.com>
2023-09-19 14:17:39 -0400

    Added epos4 folder + logger files

--- GuitarBot2023Files/src/def.h
+++ GuitarBot2023Files/src/def.h
@@ -4,11 +4,13 @@
 #define NUM_SLIDERS 6
 #define NUM_PRESSERS 6
 
-//const int kStrikerDirection[9] = { 0, 0, 1, 0, 0, 1, 1, 1, 0 }; // 0 is normal, 1 is flipped, idx 0 is dummy ????
+//const int kStrikerDirection[9] = { 0, 0, 1, 0, 0, 1, 1, 1, 0 }; // 0 is normal, 1 is flipped, idx 0 is dummy
+//needed?^^
 
 #define HOME_POSITION 1 //where would we find info to encode positions?
 
-//#define CLEAR_FAULT_TIMER_INTERVAL 100   // ms ????
+//#define CLEAR_FAULT_TIMER_INTERVAL 100   // ms
+//needed?^^
 
 //what info needed to get motor to press? Uses positioning to press or force?
 
